b729bb7f59ea3c6045f8886fc832bf1d0d7a3a51
LartyHD <lartyhd@gmail.com>
2019-06-30 01:32:41 +0200

    Added area placement on startup

--- src/main/kotlin/net/darkdevelopers/magmarun/MagmaRun.kt
+++ src/main/kotlin/net/darkdevelopers/magmarun/MagmaRun.kt
@@ -3,7 +3,10 @@ package net.darkdevelopers.magmarun
 import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
 import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.World
+import kotlin.random.Random
 
 class MagmaRun : Module {
 
@@ -16,7 +19,16 @@ class MagmaRun : Module {
 
     override fun start() {
 
-        val world: World = Bukkit.getWorlds().get(0)
+        val world: World = Bukkit.getWorlds()[0]
+
+        for (x in 0 until 50)
+            for (z in 0 until 50) {
+                val block = Location(world, x.toDouble(), 100.0, z.toDouble()).block
+                block.type = Material.WOOL
+                @Suppress("DEPRECATION")
+                block.data = Random.nextBits(4).toByte()
+            }
+
 
         println("${description.name} started")
     }
