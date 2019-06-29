55cb8f7b42d361b403695a7b9ff33aba3b35e0a3
LartyHD <lartyhd@gmail.com>
2019-06-30 01:50:51 +0200

    Moved placement spawning to a function

--- src/main/kotlin/net/darkdevelopers/magmarun/MagmaRun.kt
+++ src/main/kotlin/net/darkdevelopers/magmarun/MagmaRun.kt
@@ -5,32 +5,42 @@ import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
-import org.bukkit.World
 import kotlin.random.Random
 
+/**
+ * Created on 30.06.2019 01:37.
+ * @author Lars Artmann | LartyHD
+ */
 class MagmaRun : Module {
 
     override val description: ModuleDescription = ModuleDescription(
-        MagmaRun::class.simpleName.orEmpty(),
+        javaClass.simpleName,
         "1.0.0",
-        "Finn Artmann | Pandaro003",
+        "Lars Artmann | LartyHD",
         "A Minecraft jump and run game"
     )
 
+
     override fun start() {
 
-        val world: World = Bukkit.getWorlds()[0]
+        spawnPlatform()
 
-        for (x in 0 until 50)
+    }
+
+    fun spawnPlatform() {
+
+        val world = Bukkit.getWorlds()[0]
+        for (x in 0 until 50) {
             for (z in 0 until 50) {
                 val block = Location(world, x.toDouble(), 100.0, z.toDouble()).block
+
                 block.type = Material.WOOL
                 @Suppress("DEPRECATION")
                 block.data = Random.nextBits(4).toByte()
+
             }
+        }
 
-
-        println("${description.name} started")
     }
 
 }
