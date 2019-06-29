206f6acaca602a3ee84794384714ee20a86d27c6
LartyHD <lartyhd@gmail.com>
2019-06-30 01:59:25 +0200

    Added global val `firstWorld`

--- src/main/kotlin/net/darkdevelopers/magmarun/MagmaRun.kt
+++ src/main/kotlin/net/darkdevelopers/magmarun/MagmaRun.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
-import org.bukkit.Bukkit
+import net.darkdevelopers.magmarun.functions.firstWorld
 import org.bukkit.Location
 import org.bukkit.Material
 import kotlin.random.Random
@@ -33,10 +33,9 @@ class MagmaRun : Module {
 
         GlobalScope.launch {
 
-            val world = Bukkit.getWorlds()[0]
             for (x in 0 until 50) {
                 for (z in 0 until 50) {
-                    val block = Location(world, x.toDouble(), 100.0, z.toDouble()).block
+                    val block = Location(firstWorld, x.toDouble(), 100.0, z.toDouble()).block
 
                     block.type = Material.WOOL
                     @Suppress("DEPRECATION")
---
+++ src/main/kotlin/net/darkdevelopers/magmarun/functions/Bukkit.kt
@@ -0,0 +1,11 @@
+package net.darkdevelopers.magmarun.functions
+
+import org.bukkit.Bukkit
+import org.bukkit.World
+
+/*
+ * Created on 30.06.2019 01:56.
+ * @author Lars Artmann | LartyHD
+ */
+
+val firstWorld: World get() = Bukkit.getWorlds()[0]
